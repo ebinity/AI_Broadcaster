99dcae3adc84de92dd5a1b14c02cb41d25e2dec2
atsnokoto <80885477+atsnokoto@users.noreply.github.com>
2023-03-08 22:44:27 +0200

    Beta version

--- javascript.js
+++ javascript.js
@@ -1,46 +1,45 @@
+var listOfStreams = ["unlimitedsteam", "alwaysbreaktime"];
 
-/*
-var listOfStreams = ["unlimitedsteam", "alwaysbreaktime"]
+// Create the Twitch embed object
+var embed = new Twitch.Embed("twitch-embed", {
+  width: "100%",
+  height: "100%",
+  layout: "video",
+  allowfullscreen: true,
+  "allow-modals": true,
+  channel: listOfStreams[0] // Set initial channel to the first item in the array
+});
+
+// Define a variable to keep track of the current stream index
+var currentStreamIndex = 0;
+
+// Create a function to change the channel
+function changeChannel() {
+
+  // Increment the current stream index
+  currentStreamIndex++;
+  // If the index is greater than the length of the array, reset it to 0
+  if (currentStreamIndex >= listOfStreams.length) {
+    currentStreamIndex = 0;
+  }
+  // Set the channel value to the current stream
+  embed.setChannel(listOfStreams[currentStreamIndex]);
+  document.getElementById("nextShowTitle").innerHTML = listOfStreams[currentStreamIndex];
 
 
-new Twitch.Embed("twitch-embed", {
-    width: "100%",
-    height: "100%",
-    layout: "video",
-    allowfullscreen: true,
-    channel: "unlimitedsteam",
+  // Add the "move-animation" class to the "#nextProgram" div element
+  var nextProgramElement = document.getElementById("nextProgram");
+  nextProgramElement.classList.add("move-animation");
 
-  });
-  */
+  // Remove the "move-animation" class after 14 seconds
+  setTimeout(function() {
+    nextProgramElement.classList.remove("move-animation");
+  }, 14000);
 
-        // Define the list of streams
-        var listOfStreams = ["unlimitedsteam", "alwaysbreaktime"];
+}
 
-        // Shuffle the array function
-        function shuffle(array) {
-          for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-          }
-          return array;
-        }
-  
-        // Create the Twitch embed object
-        var embed = new Twitch.Embed("twitch-embed", {
-          width: "100%",
-          height: "100%",
-          layout: "video",
-          allowfullscreen: true,
-          channel: listOfStreams[0], // Set initial channel
-        });
-  
-        // Function to shuffle channel every 5 minutes
-        function shuffleChannel() {
-          // Shuffle the list of streams
-          shuffle(listOfStreams);
-          // Set the new channel
-          embed.setChannel(listOfStreams[0]);
-        }
-  
-        // Call shuffleChannel function every 5 minutes
-        setInterval(shuffleChannel, 30000); // 300000 milliseconds = 5 minutes
+
+
+// Call the changeChannel function after a set interval (e.g., every 10 seconds)
+setInterval(changeChannel, 30000);
+
